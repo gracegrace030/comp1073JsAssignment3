e9590b8ebe86746aca557377f2d3f066f086bf65
syanismu <113151292+syanismu@users.noreply.github.com>
2023-08-04 14:59:50 -0400

    Part 1-6 JS Done

--- script/main.js
+++ script/main.js
@@ -1,87 +1,46 @@
-// Audio
-const soundEating = document.getElementById("sound-eating");
-// This causes sound delay:
-// const soundEating = new Audio("./Voicy_Eating_sound_effect.mp3");
-
 const btnAddItem = document.getElementById("btn-add-item");
 const inputItem = document.getElementById("input-item");
 const toDoList = document.querySelector("#to-do-list ul");
-const completeList = document.querySelector("#completed-list ul");
+
 
 btnAddItem.addEventListener("click", () => {
+  var checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.setAttribute("id", "checkbox")
+
+  var checkBoxLabel = document.createElement("label");
+  checkBoxLabel.setAttribute("type", "label");
+  checkBoxLabel.setAttribute("for", "checkbox");
+
+  let li = document.createElement("li");
   inputValue = inputItem.value;
+
   if (inputValue !== null && inputValue !== "") {
-    
-    let li = document.createElement("li");
-    toDoList.appendChild(li);
 
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox")
-    li.appendChild(checkbox);
-    checkbox.addEventListener("click", (e)=>{
-      if (e.target.checked){
-        // Replay sound if sound is playing
-        soundEating.load();
-        // Add pac man effect when checkbox is on
-        pacMan(e);
-        setTimeout(()=>{
-          // List item be moved to bottom
-          removePacMan(e);
-          completeList.appendChild(li);
-        },1600);
+    checkBoxLabel.innerHTML = inputValue;
 
-      }
-      else {
-        toDoList.appendChild(li);
-      }
-    });
+    toDoList.insertBefore(li, toDoList.children[0]);
+    li.appendChild(checkBox);
+    li.appendChild(checkBoxLabel);
 
-    let span = document.createElement("span");
-    span.innerHTML = inputValue;
-    li.appendChild(span);
-    
     let btnDelete = document.createElement("button");
-    btnDelete.innerHTML = "X";
+    btnDelete.innerHTML = "Delete";
     li.appendChild(btnDelete);
     btnDelete.addEventListener("click", () => {
-      li.parentNode.removeChild(li);
+      toDoList.removeChild(li);
     });
-    
-    // Clear input field
+
     inputItem.value = "";
     inputItem.focus();
   }
+
+  checkBox.addEventListener('change', event => {
+    if(event.target.checked){
+      checkBoxLabel.style.textDecoration = "line-through";
+      toDoList.appendChild(li);
+    } else {
+      checkBoxLabel.style.textDecoration = "none";
+    }
+  
+  });
 });
-
-// Pac Man Effect ----------------------------------
-function pacMan(e){
-  // Add a pac man
-  const target = e.target;
-  const parent = target.parentNode;
-
-  const pacManWrapper = document.createElement("div");
-  pacManWrapper.setAttribute("class", "pac-man-wrapper");
-  parent.appendChild(pacManWrapper);
-
-  const pacManBg = document.createElement("div");
-  pacManBg.setAttribute("class", "pac-man-bg");
-  pacManWrapper.appendChild(pacManBg);
-
-  const pacMan = document.createElement("div");
-  pacMan.setAttribute("class", "pac-man");
-  pacManWrapper.appendChild(pacMan);
-
-  // play sound
-  soundEating.play();
-}
-
-function removePacMan(e){
-  // Remove a pac man
-  const target = e.target;
-  const parent = target.parentNode;
-  const pacManWrapper = parent.querySelector(".pac-man-wrapper");
-  parent.removeChild(pacManWrapper);
-}
-
-
-
